96785444e85c7e3fe35185d774a16855850d20a5
Konstantinos Kagkelidis <kaggis@gmail.com>
2023-07-06 13:08:56 +0300

    CAT-81 Minor UI fixes on validations, profile and footer

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { UserAPI } from '../api';
 import { AuthContext } from '../auth/AuthContext';
-import { FaAddressCard, FaPlus, FaLock } from 'react-icons/fa';
+import { FaAddressCard, FaPlus, FaLock, FaCheckCircle, FaShieldAlt } from 'react-icons/fa';
 
 
 function Profile() {
@@ -18,7 +18,9 @@ function Profile() {
   }, [profileData]);
 
   // check if the user has details
-  const hasDetails = (userProfile?.name  && userProfile?.surname  && userProfile?.name )
+  const hasDetails = (userProfile?.name  && userProfile?.surname  && userProfile?.name ) || userProfile?.user_type === "Admin"
+
+
   if (keycloak?.token && authenticated) {
     return (
       <div className="mt-4">
@@ -28,7 +30,20 @@ function Profile() {
           <header className="col-3 h4 text-muted">Account</header>
           <section className="col-9">
             <div><strong>id:</strong> {userProfile?.id}</div>
-            <div><strong>type:</strong> {userProfile?.user_type}</div>
+           
+            <div><strong>type:</strong> 
+              {userProfile?.user_type === "Identified" &&
+                <span className="m-2">{userProfile?.user_type}</span>
+              }
+              {userProfile?.user_type === "Validated" &&
+                <span className="badge bg-success m-2"><FaCheckCircle /> {userProfile?.user_type}</span>
+              }
+              {userProfile?.user_type === "Admin" &&
+                <span className="badge bg-primary m-2"><FaShieldAlt /> {userProfile?.user_type}</span>
+              }
+
+            </div>
+            
             <div><strong>registered on:</strong> {userProfile?.registered_on}</div>
           </section>
         </div>
@@ -58,6 +73,23 @@ function Profile() {
                 <Link to="/validations" className="btn btn-light border-black" >View List</Link>
                 <Link to="/validations/request" className="btn btn-light border-black mx-3" ><FaPlus /> Create New</Link>
               </div>
+            </>
+            }
+          </section>
+        </div>
+        <div className="row border-top py-3 mt-4 mb-4">
+          <header className="col-3 h4 text-muted">Assesments</header>
+          <section className="col-9 disabled">
+            
+            { !(userProfile?.user_type === "Validated" || userProfile?.user_type === "Admin") && 
+            <div className="alert alert-warning" role="alert">
+              <FaLock/ > You should be a validated user in order to be able to create validation requests
+            </div> 
+            } 
+            { (userProfile?.user_type === "Validated" || userProfile?.user_type === "Admin") &&
+            <>
+              <div className="text-muted">View your current assesments or create a new one.<br/><span className="badge bg-info">Under construction</span></div>
+
             </>
             }
           </section>
