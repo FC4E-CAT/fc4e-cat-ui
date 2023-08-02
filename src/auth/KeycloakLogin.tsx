f0faaea40afb414792e8bfca825ed92852ef7ec3
Kostis Triantafyllakis <ctriant@admin.grnet.gr>
2023-08-02 15:00:01 +0300

    Check if user is already registered when trying to register
    Signed-off-by: Kostis Triantafyllakis <ctriant@admin.grnet.gr>

--- src/auth/KeycloakLogin.tsx
+++ src/auth/KeycloakLogin.tsx
@@ -1,52 +1,69 @@
-import { useContext, useEffect } from 'react';
-import {
-    Navigate
-} from "react-router-dom";
-import { AuthContext } from './AuthContext';
-import Keycloak from 'keycloak-js';
-import KeycloakConfig from '../keycloak.json';
-import { UserAPI } from '../api';
-
+import { useContext, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+import Keycloak from "keycloak-js";
+import KeycloakConfig from "../keycloak.json";
+import { UserAPI } from "../api";
 
 const KeycloakLogin = () => {
-    const { authenticated, setAuthenticated, keycloak, setKeycloak, registered, setRegistered } = useContext(AuthContext)!;
+  const {
+    authenticated,
+    setAuthenticated,
+    keycloak,
+    setKeycloak
+  } = useContext(AuthContext)!;
 
-    useEffect(() => {
-        const initializeKeycloak = async () => {
-            const keycloakInstance = new Keycloak(KeycloakConfig);
-            try {
-                const authenticated = await keycloakInstance.init({
-                    scope: 'openid voperson_id',
-                    onLoad: 'login-required',
-                    checkLoginIframe: false,
-                    pkceMethod: 'S256'
-                });
-                setKeycloak(keycloakInstance);
-                setAuthenticated(authenticated);
-            } catch (error) {
-                console.error('Failed to initialize Keycloak:', error);
-            }
-        };
+  const {
+    refetch: getProfile,
+    isError: isErrorUserProfile,
+    isSuccess,
+  } = UserAPI.useGetProfile({
+    token: keycloak?.token,
+  });
 
-        initializeKeycloak();
-    }, [setAuthenticated, setKeycloak]);
+  const { mutateAsync: userRegister, isSuccess: isSuccessRegister } = UserAPI.useUserRegister();
 
-    const { data } = UserAPI.useUserRegister(
-        { token: keycloak?.token, isRegistered: registered}
-    );
+  useEffect(() => {
+    const initializeKeycloak = async () => {
+      const keycloakInstance = new Keycloak(KeycloakConfig);
+      try {
+        const authenticated = await keycloakInstance.init({
+          scope: "openid voperson_id",
+          onLoad: "login-required",
+          checkLoginIframe: false,
+          pkceMethod: "S256",
+        });
+        setKeycloak(keycloakInstance);
+        setAuthenticated(authenticated);
+      } catch (error) {
+        console.error("Failed to initialize Keycloak:", error);
+      }
+    };
 
-    useEffect(() => {
-        if (typeof data === "boolean") {
-            setRegistered(data);
-        }
-    }, [data, setRegistered]);
+    initializeKeycloak();
+  }, [setAuthenticated, setKeycloak]);
 
-    if (authenticated && registered) {
-        return <Navigate to="/profile" replace={true} />;
+  useEffect(() => {
+    if (keycloak?.token) {
+      if (authenticated) {
+        getProfile();
+      }
     }
-    else {
-        return <></>
+  }, [authenticated, isSuccessRegister, getProfile, keycloak]);
+
+  useEffect(() => {
+    if (isErrorUserProfile) {
+      const data: any = { token: keycloak?.token };
+      userRegister(data);
     }
+  }, [isErrorUserProfile, keycloak, userRegister]);
+
+
+  if (authenticated && isSuccess) {
+    return <Navigate to="/profile" replace={true} />;
+  } else {
+    return <></>;
+  }
 };
 
 export { KeycloakLogin };
