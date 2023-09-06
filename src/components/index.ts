eb7e86da546410b2ed15d336c4ee52ba201da6bc
Kostis Triantafyllakis <kostastriantaf@gmail.com>
2023-09-06 16:04:31 +0300

    Create a custom card component for actors

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { Header } from "./Header"
 import { Footer } from "./Footer"
 import { CustomTable } from "./CustomTable"
 import { Alert } from "./Alert"
+import { ActorCard } from "./ActorCard"
 
-export { Header, CustomTable, Footer, Alert };
+export { Header, CustomTable, Footer, Alert, ActorCard };
