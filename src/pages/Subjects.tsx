b09b466444efe40d3203c6e0d19824b3536f4b44
Konstantinos Kagkelidis <kaggis@gmail.com>
2023-12-08 15:38:22 +0200

    CAT-282: Refactor Subjects view

--- src/pages/Subjects.tsx
+++ src/pages/Subjects.tsx
@@ -1,10 +1,18 @@
 import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { ColumnDef } from "@tanstack/react-table";
 import { CustomTable } from "@/components";
-import { FaCubes, FaPlus, FaPlusCircle } from "react-icons/fa";
+import { FaCubes, FaInfoCircle, FaPlus, FaPlusCircle } from "react-icons/fa";
 import { AlertInfo, Subject } from "@/types";
 import { useCreateSubject, useGetSubjects } from "@/api/services/subjects";
-import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import {
+  Button,
+  Form,
+  InputGroup,
+  Modal,
+  OverlayTrigger,
+  Row,
+  Tooltip,
+} from "react-bootstrap";
 import { AuthContext } from "@/auth";
 import toast from "react-hot-toast";
 import { useLocation } from "react-router-dom";
@@ -54,51 +62,94 @@ export function SubjectModal(props: SubjectModalProps) {
       <Modal.Header className="bg-light" closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           <FaPlusCircle className="me-2" />
-          {`${props.editMode ? "Edit" : "Create new"} Object`}
+          {`${props.editMode ? "Edit" : "Create new"} Subject`}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group as={Row} className="mb-3" controlId="formSubjectName">
-            <Form.Label column sm={2}>
-              <strong>Name</strong>
-            </Form.Label>
-            <Col sm={10}>
+          <Row>
+            <InputGroup className="mb-3">
+              <OverlayTrigger
+                key="top"
+                placement="top"
+                overlay={
+                  <Tooltip id={`tooltip-top`}>
+                    A unique identifier for the current subject - this can be a
+                    URL, a string representing the service or organisation being
+                    assessed, or the PID of a resource owned by you.
+                  </Tooltip>
+                }
+              >
+                <InputGroup.Text id="label-info-subject-id">
+                  <FaInfoCircle className="me-2" /> Subject ID (*)
+                </InputGroup.Text>
+              </OverlayTrigger>
               <Form.Control
-                type="text"
-                placeholder="Name of the object"
+                id="input-info-subject-id"
+                placeholder="A unique identifier for the current subject"
+                value={data.subject_id || ""}
+                onChange={(e) => {
+                  handleChangeData("subject_id", e.target.value);
+                }}
+                aria-describedby="label-info-subject-id"
+              />
+            </InputGroup>
+          </Row>
+          <Row>
+            <InputGroup className="mb-3">
+              <OverlayTrigger
+                key="top"
+                placement="top"
+                overlay={
+                  <Tooltip id={`tooltip-top`}>
+                    The name of the subject of the assessment as identified
+                    above
+                  </Tooltip>
+                }
+              >
+                <InputGroup.Text id="label-info-subject-name">
+                  <FaInfoCircle className="me-2" /> Subject Name (*)
+                </InputGroup.Text>
+              </OverlayTrigger>
+              <Form.Control
+                id="input-info-subject-name"
+                placeholder="The name of the subject of the assessment as identified above"
                 value={data.name}
-                onChange={(e) => handleChangeData("name", e.target.value)}
+                onChange={(e) => {
+                  handleChangeData("name", e.target.value);
+                }}
+                aria-describedby="label-info-subject-name"
               />
-            </Col>
-          </Form.Group>
-
-          <Form.Group as={Row} className="mb-3" controlId="formSubjectType">
-            <Form.Label column sm={2}>
-              <strong>Type</strong>
-            </Form.Label>
-            <Col sm={10}>
+            </InputGroup>
+          </Row>
+          <Row>
+            <InputGroup className="mb-3">
+              <OverlayTrigger
+                key="top"
+                placement="top"
+                overlay={
+                  <Tooltip id={`tooltip-top`}>
+                    The type of object (such as a web resource identified by the
+                    owner) or service provided by an authority, provider, or
+                    manager, for which the assessment will be completed.
+                  </Tooltip>
+                }
+              >
+                <InputGroup.Text id="label-info-subject-type">
+                  <FaInfoCircle className="me-2" /> Subject Type (*)
+                </InputGroup.Text>
+              </OverlayTrigger>
               <Form.Control
-                type="text"
-                placeholder="Specify the type of object (e.g. service)"
+                id="input-info-subject-type"
+                placeholder="The type of object for which the assessment will  be completed"
                 value={data.type}
-                onChange={(e) => handleChangeData("type", e.target.value)}
+                onChange={(e) => {
+                  handleChangeData("type", e.target.value);
+                }}
+                aria-describedby="label-info-subject-type"
               />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3" controlId="formSubjectId">
-            <Form.Label column sm={2}>
-              <strong>Id</strong> <em>(optional)</em>
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                placeholder="Specify a reference id to this object (e.g. a PID)"
-                value={data.subject_id}
-                onChange={(e) => handleChangeData("subject_id", e.target.value)}
-              />
-            </Col>
-          </Form.Group>
+            </InputGroup>
+          </Row>
         </Form>
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-between">
@@ -106,7 +157,7 @@ export function SubjectModal(props: SubjectModalProps) {
           Cancel
         </Button>
         <Button
-          className="btn-success me-4"
+          className="btn-success"
           onClick={() => {
             props.editMode ? props.onUpdate(data) : props.onCreate(data);
           }}
@@ -145,24 +196,24 @@ function Subjects() {
   const cols = useMemo<ColumnDef<Subject>[]>(
     () => [
       {
-        accessorFn: (row) => row.type,
-        id: "type",
+        accessorFn: (row) => row.subject_id,
+        id: "subject_id",
         cell: (info) => info.getValue(),
-        header: () => <span>Type</span>,
+        header: () => <span>Subject Id</span>,
         enableColumnFilter: false,
       },
       {
         accessorFn: (row) => row.name,
         id: "name",
         cell: (info) => info.getValue(),
-        header: () => <span>Name</span>,
+        header: () => <span>Subject Name</span>,
         enableColumnFilter: false,
       },
       {
-        accessorFn: (row) => row.subject_id,
-        id: "subject_id",
+        accessorFn: (row) => row.type,
+        id: "type",
         cell: (info) => info.getValue(),
-        header: () => <span>Subject Id</span>,
+        header: () => <span>Subject Type</span>,
         enableColumnFilter: false,
       },
     ],
@@ -181,13 +232,13 @@ function Subjects() {
       .mutateAsync(item)
       .catch((err) => {
         toastAlert.current = {
-          message: "Error during assessment creation.",
+          message: "Error during subject creation.",
         };
         throw err;
       })
       .then(() => {
         toastAlert.current = {
-          message: "Assessment succesfully created.",
+          message: "Subject succesfully created.",
         };
         // close the modal
         setSubjectModalConfig((prevConfig) => ({ ...prevConfig, show: false }));
@@ -212,7 +263,7 @@ function Subjects() {
       <div className={"d-flex justify-content-between container"}>
         <h3 className="cat-view-heading">
           <FaCubes className="me-2" />
-          my objects
+          subjects
         </h3>
         <Button
           variant="light"
