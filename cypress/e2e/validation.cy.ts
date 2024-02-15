dc0f2108751471e00b5360f6c7c019868d02bed2
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2024-02-15 16:40:48 +0100

    Created spec for admin validation creation. Updated validation spec and commands to use correct fixture location.

--- cypress/e2e/validation.cy.ts
+++ cypress/e2e/validation.cy.ts
@@ -9,10 +9,30 @@ describe("/validations/request", () => {
 
   it("creates a validation", () => {
     cy.intercept("POST", "http://localhost:8080/v1/validations", {
-      fixture: "validation_review.json",
+      fixture: "validations/validation_review.json",
     }).as("getValidations");
     cy.get(".select__input").click().type("Oxford");
     cy.contains(".select__option", "University of Oxford").click();
+    cy.get("#actor_id").select(1);
+    cy.get("#organisation_role").type("Team Manager");
+    cy.get("#actor_id")
+      .should("have.value", "1")
+      .contains("PID Standards Body");
+    cy.get("#create_validation").click();
+    cy.wait(["@getValidations"]);
+    cy.url().should("contain", "/validations");
+    cy.contains("Validation request succesfully submitted.").should(
+      "be.visible",
+    );
+  });
+
+  it("creates a validation with a custom organisation", () => {
+    cy.intercept("POST", "http://localhost:8080/v1/validations", {
+      fixture: "validations/validation_review.json",
+    }).as("getValidations");
+    cy.get("#organisation_source").select("Custom");
+    cy.get("#organisation_name").type("Test Organisation");
+    cy.get("#organisation_website").type("www.example.com");
     cy.get("#actor_id").select(1);
     cy.get("#organisation_role").type("Team Manager");
     cy.get("#actor_id")
