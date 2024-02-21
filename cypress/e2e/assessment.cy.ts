10e44a0047f114edc36daad5119acc147c042e8d
Fjodor van Rijsselberg <fjodorvanrijsselberg@Fjodors-MacBook-Pro.local>
2024-02-21 13:21:02 +0100

    Extended assessment spec to test assessment filter, and edite/delete assessment.

--- cypress/e2e/assessment.cy.ts
+++ cypress/e2e/assessment.cy.ts
@@ -28,7 +28,7 @@ describe("/assessments/create", () => {
 
   it("Can go to step 2 and go back", () => {
     cy.get("#radio-0").click();
-    cy.get(".btn-success").should("not.have.attr", "disabled");
+    cy.get("#create_assessment_button").should("not.have.attr", "disabled");
     cy.get("#next-button").click();
     cy.get("#assessment-wizard-tab-step-2").should(
       "have.attr",
@@ -44,7 +44,7 @@ describe("/assessments/create", () => {
   });
   it("makes the submitter fields readonly", () => {
     cy.get("#radio-0").click();
-    cy.get(".btn-success").should("not.have.attr", "disabled");
+    cy.get("#create_assessment_button").should("not.have.attr", "disabled");
     cy.get("#next-button").click();
     cy.get('#accordion_general button[aria-expanded="true"]').should("exist");
     cy.get("#accordion_submitter").click();
@@ -80,7 +80,7 @@ describe("/assessments/create", () => {
     const subjectName = "identified_test_subject_name";
     const subjectType = "identified_test_subject_type";
     cy.get("#radio-0").click();
-    cy.get(".btn-success").should("not.have.attr", "disabled");
+    cy.get("#create_assessment_button").should("not.have.attr", "disabled");
     cy.get("#assessment-wizard-tab-step-2").click();
     cy.get("#input-info-name").type("identified_test_assessment");
     cy.get("#accordion_subject").click();
@@ -197,8 +197,107 @@ describe("/assessments/create", () => {
       .contains("PASS")
       .should("exist");
 
-    cy.get(".btn-success").click();
+    cy.get("#create_assessment_button").click();
     cy.url().should("contain", "/assessments");
     cy.contains("Assessment succesfully created.").should("be.visible");
+  });
+});
+
+describe("/assessments", () => {
+  beforeEach(() => {
+    cy.kcLogout();
+    cy.kcLogin("identified").as("identifiedTokens");
+    cy.visit("/assessments");
+  });
+
+  it("should be able to filter on subject type and name", () => {
+    cy.contains("Filter").click();
+
+    // Test subject type filtering
+    cy.get("#floatingSelectSubjectType").select("identified_test_subject_type");
+    cy.get("#floatingSelectSubjectType")
+      .find(":selected")
+      .should("have.text", "identified_test_subject_type");
+    cy.get("#apply_filter_button").click();
+    cy.get("table").find("tbody").find("tr").should("exist");
+
+    cy.get("#clear_filter_button").click();
+    cy.get("#floatingSelectSubjectType")
+      .find(":selected")
+      .should("have.text", "Select Subject Type");
+
+    // Test subject name filtering
+    cy.get("#floatingSelectSubjectName").select("identified_test_subject_name");
+    cy.get("#floatingSelectSubjectName")
+      .find(":selected")
+      .should("have.text", "identified_test_subject_name");
+    cy.get("#apply_filter_button").click();
+    cy.get("table").find("tbody").find("tr").should("exist");
+    cy.get("#clear_filter_button").click();
+    cy.get("#floatingSelectSubjectName")
+      .find(":selected")
+      .should("have.text", "Select Subject Name");
+  });
+
+  it("makes no changes to assessment when editing and not saving", () => {
+    cy.get("table")
+      .contains("td", "1")
+      .parent("tr")
+      .find('[id^="edit-button-"]')
+      .click();
+    cy.get("#actorRadio").contains("PID Manager at Oxford Fertility");
+    cy.get("#assessment-wizard-tab-step-3").click();
+    cy.get("form:visible").within(() => {
+      cy.get("#test-check-no").click();
+    });
+    cy.get("#left-tabs-tabpane-C5 .badge:visible").contains("FAIL");
+    cy.contains("Close").click();
+    // select via Ranking header to prevent breaking tests when columns change.
+    cy.contains("thead th", "Ranking").then((rankingHeader) => {
+      const rankingColumnIndex = rankingHeader.index();
+      const badgeColumnIndex = rankingColumnIndex - 1;
+
+      cy.get(`tbody > :nth-child(1) > :nth-child(${badgeColumnIndex + 1})`)
+        .find(".badge")
+        .should("contain", "PASS");
+
+      cy.get(
+        `tbody > :nth-child(1) > :nth-child(${rankingColumnIndex + 1})`,
+      ).should("contain", "7");
+    });
+  });
+
+  it("edits an assessment", () => {
+    cy.get("table")
+      .contains("td", "1")
+      .parent("tr")
+      .find('[id^="edit-button-"]')
+      .click();
+    cy.get("#actorRadio").contains("PID Manager at Oxford Fertility");
+    cy.get("#assessment-wizard-tab-step-3").click();
+    cy.get("form:visible").within(() => {
+      cy.get("#test-check-no").click();
+    });
+    cy.get("#left-tabs-tabpane-C5 .badge:visible").contains("FAIL");
+    cy.get("#submit_assessment_button").click();
+    // select via Ranking header to prevent breaking tests when columns change.
+    cy.contains("thead th", "Ranking").then((rankingHeader) => {
+      const rankingColumnIndex = rankingHeader.index();
+      cy.get(
+        `tbody > :nth-child(1) > :nth-child(${rankingColumnIndex + 1})`,
+      ).should("contain", "6");
+    });
+    cy.contains("Assessment succesfully updated.").should("be.visible");
+  });
+
+  it("deletes an assessment", () => {
+    cy.get("table")
+      .contains("td", "1")
+      .parent("tr")
+      .find('[id^="delete-button-"]')
+      .click();
+    cy.get("#contained-modal-title-vcenter").contains("Delete Assessment");
+    cy.get(".btn-danger").click();
+    cy.contains("Assessment succesfully deleted.").should("be.visible");
   });
 });
