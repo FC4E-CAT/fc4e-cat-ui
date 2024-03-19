86ea3a5faeb05b1edc96f8d6685900c2403ed65b
Konstantinos Kagkelidis <kaggis@gmail.com>
2024-03-19 11:50:46 +0200

    CAT-254 Create an about page with service description

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import imgFcat from "@/assets/fcat.jpg"
+
+function Home() {
+  return (
+    <div className="page-center">
+      <h2>Compliance Assesment Tool</h2>
+      <img src={imgFcat} />
+    </div>
+  );
+}
+
+export default Home;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div className="page-center">
+    <div className="page-center text-align-center p-4">
       <h2>Welcome to Compliance Assesment Tool</h2>
+      <Link to="/about" className="btn btn-success">Learn more...</Link>
     </div>
   );
 }
