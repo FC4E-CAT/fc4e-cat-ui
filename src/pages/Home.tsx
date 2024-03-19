5914f9357b589d99dd1d4f319a5fdf09395fec94
Konstantinos Kagkelidis <kaggis@gmail.com>
2024-03-19 14:05:35 +0200

    CAT-254 Add a description page about the service

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
+import { FaInfoCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="page-center text-align-center p-4">
+    <div className="page-center text-center p-4">
       <h2>Welcome to Compliance Assesment Tool</h2>
-      <Link to="/about" className="btn btn-success">Learn more...</Link>
+      <Link to="/about" className="btn btn-light border mt-4">
+        <FaInfoCircle className="me-2 text-muted" /> About...
+      </Link>
     </div>
   );
 }
