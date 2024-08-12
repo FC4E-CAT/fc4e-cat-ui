0d19021a45b65e4a3ad84588debdc00db1808ace
Themis Zamani <themiszamani@gmail.com>
2024-08-13 00:00:38 +0300

    update user profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -36,6 +36,51 @@ function Profile() {
           <FaAddressCard /> profile
         </h3>
 
+        <div className="container rounded bg-white mt-5 mb-5">
+          <div className="row">
+            <div className="col-3 border-right">
+              <div className="d-flex flex-column align-items-center text-center p-1 py-1">
+                <img
+                  className="rounded-circle mt-5"
+                  width="150px"
+                  src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                />
+                <span>(icon)orcid.org/0000-0001-9547-1582</span>
+                <span className="font-weight-bold">Chrisoula Thermolia</span>
+                <span className="text-black-50">
+                  chrisoulathermolia@gmail.com
+                </span>
+                <span className="badge bg-success">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    stroke-width="0"
+                    viewBox="0 0 512 512"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
+                  </svg>{" "}
+                  Validated
+                </span>
+                <span>
+                  <br />{" "}
+                </span>
+                <p className="font-italic">
+                  <i>1fe601..@einfra.grnet.gr</i>(copy icon/svg)
+                </p>
+                <span> </span>
+              </div>
+            </div>
+            <div className="col-9 border-right">
+              <div className="p-3 py-5">
+                <header className="h4 text-muted">User Dashboard</header>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="row border-top py-3 mt-4">
           <header className="col-3 h4 text-muted">Account</header>
           <section className="col-9">
