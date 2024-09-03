a3e9304d602e62901153c9f225d30ea3988350e4
Themis Zamani <themiszamani@gmail.com>
2024-09-03 09:40:58 +0300

    CAT-487:Update Assessment menu

--- src/pages/assessments/components/CriteriaTabs.tsx
+++ src/pages/assessments/components/CriteriaTabs.tsx
@@ -57,9 +57,11 @@ export function CriteriaTabs(props: CriteriaTabsProps) {
   }, [props]);
 
   props.principles.forEach((principle) => {
-    <div>
-      <h3>principle.name</h3>
-    </div>;
+    navs.push(
+      <div>
+        <h3>principle.name</h3>
+      </div>,
+    );
     principle.criteria.forEach((criterion) => {
       navs.push(
         <Nav.Item key={criterion.id} className="cat-crit-tab">
