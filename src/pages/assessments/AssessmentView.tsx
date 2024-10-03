a53306063b0e4c6187ae788a59de0800dd1ceaed
Themis Zamani <themiszamani@gmail.com>
2024-10-03 15:44:08 +0300

    CAT-520:A more user-friendly way of viewing completed assessments

--- src/pages/assessments/AssessmentView.tsx
+++ src/pages/assessments/AssessmentView.tsx
@@ -74,66 +74,82 @@ const AssessmentView = ({ isPublic }: { isPublic: boolean }) => {
             </Col>
           </Row>
           <Row>
-          <Col className="col-sm-3">
-          <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Details</Card.Title>
-        <Card.Text>
-          <span>
-              Actor: <strong>{assessment.actor.name}</strong>
-            </span>
-            <span>
-              Organization: <strong>{assessment.organisation.name}</strong>
-            </span>
-            <span>
-              Subject:{" "}
-              <strong>
-                {assessment.subject.name} - (type: {assessment.subject.type})
-              </strong>
-            </span>
-
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </Col>
-    <Col className="col-sm-9">
-            <Row>
-              <h3>Statistics</h3>
-              <Col className="text-center">
-                <span className="font-weight-500 text-xs text-gray-500">Principles</span>
-                <p><span className="fs-1 text-primary bold">9</span></p>
-              </Col>
-              <Col className="text-center">
-                <span className="font-weight-500 text-xs text-gray-500">Criteria</span>
-                <p><span className="fs-1 text-primary bold">9</span><span className="fs-6 text-secondary">/10</span></p>
-              </Col>
-              <Col className="text-center">
-                <span className="font-weight-500 text-xs text-gray-500">Mandatory</span>
-                <p><span className="fs-1 text-primary bold">8</span><span className="fs-6 text-secondary">/10</span></p>
-              </Col>
-              <Col className="col-md-auto col col-lg-1  text-center">
-                <span className="font-weight-500 text-xs text-gray-500">Optional</span>
-                <p><span className="fs-1 text-primary bold">8</span><span className="fs-6 text-secondary">/10</span></p>
-              </Col>
-            </Row>
-    
-    </Col>
+            <Col className="col-sm-3">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Details</Card.Title>
+                  <Card.Text>
+                    <span>
+                      Actor: <strong>{assessment.actor.name}</strong>
+                    </span>
+                    <span>
+                      Organization:{" "}
+                      <strong>{assessment.organisation.name}</strong>
+                    </span>
+                    <span>
+                      Subject:{" "}
+                      <strong>
+                        {assessment.subject.name} - (type:{" "}
+                        {assessment.subject.type})
+                      </strong>
+                    </span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className="col-sm-9">
+              <Row>
+                <h3>Statistics</h3>
+                <Col className="text-center">
+                  <span className="font-weight-500 text-xs text-gray-500">
+                    Principles
+                  </span>
+                  <p>
+                    <span className="fs-1 text-primary bold">9</span>
+                  </p>
+                </Col>
+                <Col className="text-center">
+                  <span className="font-weight-500 text-xs text-gray-500">
+                    Criteria
+                  </span>
+                  <p>
+                    <span className="fs-1 text-primary bold">9</span>
+                    <span className="fs-6 text-secondary">/10</span>
+                  </p>
+                </Col>
+                <Col className="text-center">
+                  <span className="font-weight-500 text-xs text-gray-500">
+                    Mandatory
+                  </span>
+                  <p>
+                    <span className="fs-1 text-primary bold">8</span>
+                    <span className="fs-6 text-secondary">/10</span>
+                  </p>
+                </Col>
+                <Col className="col-md-auto col col-lg-1  text-center">
+                  <span className="font-weight-500 text-xs text-gray-500">
+                    Optional
+                  </span>
+                  <p>
+                    <span className="fs-1 text-primary bold">8</span>
+                    <span className="fs-6 text-secondary">/10</span>
+                  </p>
+                </Col>
+              </Row>
+            </Col>
           </Row>
           <Button
-        variant="secondary my-4"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        Back
-      </Button>  
+            variant="secondary my-4"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Back
+          </Button>
         </div>
-        
       )}
       ;{/* Debug info here - display assessment json */}
       <DebugJSON assessment={assessment} />
-
-      
     </div>
   );
 };
