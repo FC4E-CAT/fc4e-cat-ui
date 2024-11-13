0f8b818378b6809ac533b3d2114ea743abf2c66c
Konstantinos Kagkelidis <kaggis@gmail.com>
2024-11-13 18:50:07 +0200

    CAT-624 center admin view layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="page-center text-center p-4">
-      <h2>Welcome to Compliance Assesment Tool</h2>
-      <Link to="/about" className="btn btn-light border mt-4">
-        <FaInfoCircle className="me-2 text-muted" /> About...
-      </Link>
+    <div className="container rounded bg-white mt-1 mb-5">
+      <div className="page-center text-center p-4">
+        <h2>Welcome to Compliance Assesment Tool</h2>
+        <Link to="/about" className="btn btn-light border mt-4">
+          <FaInfoCircle className="me-2 text-muted" /> About...
+        </Link>
+      </div>
     </div>
   );
 }
